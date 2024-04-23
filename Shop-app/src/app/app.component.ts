9ca576813ffc93bc95cb5e739034ab838c8c4e75
heykayk <ngohieu123kayk@gmail.com>
2024-04-23 22:44:20 +0700

    detail-product

--- Shop-app/src/app/app.component.ts
+++ Shop-app/src/app/app.component.ts
@@ -5,11 +5,18 @@ import { OrderComponent } from './order/order.component';
 import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { DetailProductComponent } from './detail-product/detail-product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, OrderComponent, OrderConfirmComponent, LoginComponent, RegisterComponent],
+  imports: [RouterOutlet, 
+            HomeComponent, 
+            OrderComponent, 
+            OrderConfirmComponent, 
+            LoginComponent, 
+            RegisterComponent,
+            DetailProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
